2910b811ec454170843cf2814dc9ca8d438d40af
Yuliia <coconut3.dev@gmail.com>
2024-05-28 14:05:48 +0300

    Theme fix 2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 VERSION = '0.2.0'
 
 setup(
-    name="mkdocs-moonstone",
+    name="moonstone",
     version=VERSION,
     url='https://github.com/byrnereese/mkdocs-moonstone/',
     description='Moonstone theme for MkDocs',
@@ -40,7 +40,7 @@ setup(
     python_requires='>=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     entry_points={
         'mkdocs.themes': [
-            'moonstone = mkdocs_moonstone',
+            'moonstone = moonstone',
         ]
     },
     zip_safe=False
